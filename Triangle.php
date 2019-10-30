fa0a45bc08085c7e3d526bb64e650d8ff02c87c3
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2019-10-30 11:54:24 +0530

    Update Triangle.php

--- Triangle.php
+++ Triangle.php
@@ -1,8 +1,12 @@
 <?php
 $a=10;
-echo "Value of A =".$a; 
+echo "Value of A =".$a."\n"; 
 for($i=0;$i<$a;$i++)
 {
+   for($k=$a-1;$k>$i;$k--) 
+   {
+       echo "  ";
+   }
    for($j=0;$j<=$i;$j++)
    {
       echo "* ";
